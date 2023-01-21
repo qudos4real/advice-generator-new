c2666ea2294525ee586c7335ef7eaa909bf7cfeb
Qudos4real <qudos4real@yahoo.com>
2023-01-21 06:43:32 +0100

    Added and inputted API data

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
 <div class="main-container">
-  <p class="id">Advice #21</p>
+  <p class="id">Advice #{{ num }}</p>
   <p class="quote">
-    Ad
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam obcaecati dolores delectus mollitia tenetur quam nobis ab iste amet, commodi consequuntur minus beatae facere architecto nam voluptatibus dolor similique tempore.
+    {{ advice }}
   </p>
   <img src="./assets/images/pattern-divider-mobile.svg">
   <!-- <br>
   <img src="./assets/images/pattern-divider-desktop.svg"> -->
 <div class="move">
-  <div class="dice-container">
+  <div class="dice-container" @click="getAdvice">
     <img src="./assets/images/icon-dice.svg">
   </div>
   </div>
@@ -17,10 +16,35 @@
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      num: 0,
+      advice: ''
+    }
+  },
+  methods: {
+    async getAdvice() {
+      const response = await axios.get('https://api.adviceslip.com/advice')
+      this.num = response.data.slip.id
+      this.advice = response.data.slip.advice
+      console.log(this.num)
+      console.log(this.advice)
+    }
+    // getAdvice() {
+    //   axios
+    //     .get('https://api.adviceslip.com/advice')
+    //     .then(response => (this.advice = response))
+    //     console.log(this.advice)
+  // }
 
+},
+mounted() {
+  this.getAdvice()
+}
 }
 </script>
 
